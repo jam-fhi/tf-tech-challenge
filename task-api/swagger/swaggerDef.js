b6da7cc0e90bf3d6456b916dda8fb7353deb44b9
jamie <jamiefhill@gmail.com>
2020-05-25 18:35:27 +0100

    Added express framework and swagger generation, + misc support libraries

---
+++ task-api/swagger/swaggerDef.js
@@ -0,0 +1,10 @@
+module.exports = {
+	info: {
+		title: 'TrustFLight Task Manager!',
+		version: '1.0.0',
+		description: 'Technical Challenge for Trust Flight',
+	},
+	host: 'localhost:3001',
+	apis: ['src/**/*.ts'],
+	basePath: '/api',
+};
